cf1ba2784cefbc854baaf9bc8cd4b9ad41f5b327
Rahul Singh <fxrahul@users.noreply.github.com>
2018-07-25 08:27:00 +0530

    Update dashview.blade.php

--- resources/views/dashview.blade.php
+++ resources/views/dashview.blade.php
@@ -1,309 +1,66 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+<?php
 
-        <title>Dashboard</title>
+namespace App\Http\Controllers;
 
-        <!-- Bootstrap CSS CDN -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <!-- Our Custom CSS -->
-        <link rel="stylesheet" href="style2.css">
-        <!-- Scrollbar Custom CSS -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css">
-
-<style>
-  
-  /*
-    DEMO STYLE
-*/
-@import "https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700";
-
-
-body {
-    font-family: 'Poppins', sans-serif;
-    background: #fafafa;
-}
-
-p {
-    font-family: 'Poppins', sans-serif;
-    font-size: 1.1em;
-    font-weight: 300;
-    line-height: 1.7em;
-    color: #999;
-}
-
-a, a:hover, a:focus {
-    color: inherit;
-    text-decoration: none;
-    transition: all 0.3s;
-}
-
-.navbar {
-    padding: 15px 10px;
-    background: #fff;
-    border: none;
-    border-radius: 0;
-    margin-bottom: 40px;
-    box-shadow: 1px 1px 3px rgba(0, 0, 0, 0.1);
-}
-
-.navbar-btn {
-    box-shadow: none;
-    outline: none !important;
-    border: none;
-}
-
-.line {
-    width: 100%;
-    height: 1px;
-    border-bottom: 1px dashed #ddd;
-    margin: 40px 0;
-}
-
-/* ---------------------------------------------------
-    SIDEBAR STYLE
------------------------------------------------------ */
-#sidebar {
-    width: 250px;
-    position: fixed;
-    top: 0;
-    left: 0;
-    height: 100vh;
-    z-index: 999;
-    background: #7386D5;
-    color: #fff;
-    transition: all 0.3s;
-}
-
-#sidebar.active {
-    margin-left: -250px;
-}
-
-#sidebar .sidebar-header {
-    padding: 20px;
-    background: #6d7fcc;
-}
-
-#sidebar ul.components {
-    padding: 20px 0;
-    border-bottom: 1px solid #47748b;
-}
-
-#sidebar ul p {
-    color: #fff;
-    padding: 10px;
-}
-
-#sidebar ul li a {
-    padding: 10px;
-    font-size: 1.1em;
-    display: block;
-}
-#sidebar ul li a:hover {
-    color: #7386D5;
-    background: #fff;
-}
-
-#sidebar ul li.active > a, a[aria-expanded="true"] {
-    color: #fff;
-    background: #6d7fcc;
-}
-
-
-a[data-toggle="collapse"] {
-    position: relative;
-}
-
-a[aria-expanded="false"]::before, a[aria-expanded="true"]::before {
-    content: '\e259';
-    display: block;
-    position: absolute;
-    right: 20px;
-    font-family: 'Glyphicons Halflings';
-    font-size: 0.6em;
-}
-a[aria-expanded="true"]::before {
-    content: '\e260';
-}
-
-
-ul ul a {
-    font-size: 0.9em !important;
-    padding-left: 30px !important;
-    background: #6d7fcc;
-}
-
-ul.CTAs {
-    padding: 20px;
-}
-
-ul.CTAs a {
-    text-align: center;
-    font-size: 0.9em !important;
-    display: block;
-    border-radius: 5px;
-    margin-bottom: 5px;
-}
-a.download {
-    background: #fff;
-    color: #7386D5;
-}
-a.article, a.article:hover {
-    background: #6d7fcc !important;
-    color: #fff !important;
-}
-
-
-/* ---------------------------------------------------
-    CONTENT STYLE
------------------------------------------------------ */
-#content {
-    width: calc(100% - 250px);
-    padding: 40px;
-    min-height: 100vh;
-    transition: all 0.3s;
-    position: absolute;
-    top: 0;
-    right: 0;
-}
-#content.active {
-    width: 100%;
-}
-
-
-/* ---------------------------------------------------
-    MEDIAQUERIES
------------------------------------------------------ */
-@media (max-width: 768px) {
-    #sidebar {
-        margin-left: -250px;
+use Illuminate\Http\Request;
+use App\Models\DashModel;
+use Illuminate\Support\Facades\Auth;
+class DashController extends Controller
+{
+    //
+    public function dashboard(Request $request,$s_id){
+        $result=DashModel::where('soc_id', $s_id)->paginate(10);
+        // $m_maint_no=$result['m_maint_no'];
+        // $m_name=$result['m_name'];
+        // $m_address=$result['m_address'];
+        // $m_pan_card=$result['m_pan_card'];
+        // $m_cel_no=$result['m_cel_no'];
+        // $m_parking_charge=$result['m_parking_charge'];
+        // $m_gas_charge=$result['m_gas_charge'];
+        // $m_water_charge=$result['m_water_charge'];
+        // $m_light_charge=$result['m_light_charge'];
+        return view('dashview',['result'=>$result,'soc_id'=>$s_id]);
+        // ['soc_id'=>$s_id,'m_maint_no'=>$m_maint_no,'m_name'=>$m_name,
+        // 'm_address'=>$m_address,'m_pan_card'=>$m_pan_card,'m_cel_no'=>$m_cel_no,
+        // 'm_parking_charge'=>$m_parking_charge,'m_gas_charge'=>$m_gas_charge,'m_water_charge'=>$m_water_charge,
+        // 'm_light_charge'=>$m_light_charge]);
     }
-    #sidebar.active {
-        margin-left: 0;
+    public function store(Request $request){
+        $soc_id=$request->input('soc_id');
+        $m_maint_no=$request->input('m_maint_no');
+        $m_name=$request->input('m_name');
+        $m_email=$request->input('m_email');
+        $m_password=$request->input('m_password');
+        $m_wing=$request->input('m_wing');
+        $m_flat_no=$request->input('m_flat_no');
+        $m_address=$request->input('m_address');
+        $m_pan_card=$request->input('m_pan_card');
+        $m_cel_no=$request->input('m_cel_no');
+        $m_parking_charge='5000';
+        $m_gas_charge='5000';
+        $m_water_charge='5000';
+        $m_light_charge='5000';
+        $result=DashModel::create(['soc_id'=>$soc_id,'m_maint_no'=>$m_maint_no,'m_name'=>$m_name,'m_email'=>$m_email,
+                                    'm_password'=>$m_password,'m_wing'=>$m_wing,'m_flat_no'=>$m_flat_no,'m_address'=>$m_address,
+                                    'm_pan_card'=>$m_pan_card,
+                                    'm_cel_no'=>$m_cel_no,'m_parking_charge'=>$m_parking_charge,
+                                    'm_gas_charge'=>$m_gas_charge,
+                                    'm_water_charge'=>$m_water_charge,'m_light_charge'=>$m_light_charge]);
+        return view('registermembersuccess');
     }
-    #content {
-        width: 100%;
+    public function check(Request $request){
+        $m_email=$request->input('email');
+        $m_password=$request->input('password');
+        $result=DashModel::where('m_email',$m_email)->where('m_password',$m_password)->first();
+        if($result){
+            return view('member_dash',['result'=>$result]);
+        }
+        else{
+            return view('memberlogin');
+        }
     }
-    #content.active {
-        width: calc(100% - 250px);
-    }
-    #sidebarCollapse span {
-        display: none;
+    public function logout(Request $request){
+        Auth::logout();
+        return view('welcome');
     }
 }
-
-</style>
-    </head>
-    <body>
-
-
-
-        <div class="wrapper">
-            <!-- Sidebar Holder -->
-            <nav id="sidebar">
-                <div class="sidebar-header">
-                    <h3>Welcome Admin </h3></h3>
-                </div>
-
-                <ul class="list-unstyled components">
-                    <p>Society Number:&nbsp;{{$soc_id}}</p>
-                    <li class="active">
-                        <a href="#menu" >Dashboard</a>
-                        
-                    </li>
-                    <li>
-                        <a href="#menu">Parking Rate</a>
-                        
-                        
-                    </li>
-                    <li>
-                        <a href="#">Water Rate</a>
-                    </li>
-                    <li>
-                        <a href="#">Gas Rate</a>
-                    </li>
-                   
-                    
-                    
-                </ul>
-
-                
-            </nav>
-
-            <!-- Page Content Holder -->
-            <div id="content">
-
-                <nav class="navbar navbar-default">
-                    <div class="container-fluid">
-
-                        <div class="navbar-header">
-                            <button type="button" id="sidebarCollapse" class="btn btn-info navbar-btn">
-                                <i class="glyphicon glyphicon-align-left"></i>
-                                <span>Hide Sidebar</span>
-                            </button>
-                        </div>
-
-                        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                            <ul class="nav navbar-nav navbar-right">
-                                <li><a href="{{url('lgout')}}">Logout</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
-                <!-- table starts here -->
-                <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">Maintenance No</th>
-      <th scope="col">Name</th>
-      <th scope="col">Address</th>
-      <th scope="col">Pan Card</th>
-      <th scope="col">Mobile</th>
-      <th scope="col">Parking Charge</th>
-      <th scope="col">Gas Charge</th>
-      <th scope="col">Water Charge</th>
-      <th scope="col">Light Charge</th>
-    </tr>
-  </thead>
-  <tbody>
-    @foreach ($result as $i)
-    <tr>
-      <td>{{$i->m_maint_no}}</td>
-      <td>{{$i->m_name}}</td>
-      <td>{{$i->m_address}}</td>
-      <td>{{$i->m_pan_card}}</td>
-      <td>{{$i->m_cel_no}}</td>
-      <td>{{$i->m_parking_charge}}</td>
-      <td>{{$i->m_gas_charge}}</td>
-      <td>{{$i->m_water_charge}}</td>
-      <td>{{$i->m_light_charge}}</td>
-    </tr>
-    @endforeach
-  </tbody>
-</table>
-            </div>
-        </div>
-
-        <!-- jQuery CDN -->
-        <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
-        <!-- Bootstrap Js CDN -->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        <!-- jQuery Custom Scroller CDN -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.concat.min.js"></script>
-
-        <script type="text/javascript">
-            $(document).ready(function () {
-               
-
-                $('#sidebarCollapse').on('click', function () {
-                    $('#sidebar, #content').toggleClass('active');
-                    $('.collapse.in').toggleClass('in');
-                    $('a[aria-expanded=true]').attr('aria-expanded', 'false');
-                });
-            });
-        </script>
-    </body>
-</html>
